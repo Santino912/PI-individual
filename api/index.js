bc311cda6816bd8de9a5511689868a90854d0e73
Santino912 <santinoscolamieri@yahoo.com.ar>
2022-12-14 13:58:18 -0300

    Update to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import app from "./routes/index.js";
 import "./database/index.js";
 
-app.listen(PORT, () => {
+app.listen(`https://dogfinderapi.onrender.com`, () => {
   console.log(`Server Started in https://dogfinderapi.onrender.com`);
 });
